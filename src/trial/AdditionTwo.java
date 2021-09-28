9992bf7a9d42406b4a41d8e4b14b21d476ad035b
merhawi <mwoldemichael>
2021-09-28 17:29:14 -0500

    trwt2

--- src/trial/AdditionTwo.java
+++ src/trial/AdditionTwo.java
@@ -4,6 +4,6 @@ public class AdditionTwo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("merhawi");
-        
+        System.out.println("abel");
     }
 }
